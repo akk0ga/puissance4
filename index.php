353b00f52f201d82b9fd31430e4b840ceeb6d1ff
titouan <ttn35000@gmail.com>
2020-10-31 00:15:46 +0100

    create session and array
    session varaible and array html

--- index.php
+++ index.php
@@ -8,25 +8,14 @@
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
     ];
+    $_SESSION["player"];
+include("template/header.html");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/index.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>Puissance 4</title>
-</head>
-<body>
     <header>
         <h1 class="text-capitalize text-center">puissance 4</h1>
     </header>
     <main class="d-flex align-items-center justify-content-center">
-        <a href="#" class="btn btn-primary btn-lg btn-block text-capitalize">play !</a>
+        <a href="template/game.html" class="btn btn-primary btn-lg btn-block text-capitalize">play !</a>
     </main>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous" defer></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous" defer></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous" defer></script>
-</body>
-</html>
+<?php
+include("template/footer.html");
