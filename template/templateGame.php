8e0ffd0f16ef90b8ae643846f27ddbbde2b0dadf
titouan <ttn35000@gmail.com>
2020-11-05 01:10:05 +0100

    update v1.1.0
    update file content

--- template/templateGame.php
+++ template/templateGame.php
@@ -1,17 +1,4 @@
 <?php
-session_start();
-include ("fonctions.php");
-if(isset($_POST["submit"]) && isset($_POST["case"])){
-    //ajouter la valeur la case
-    setArrayValue($_POST["case"], $_SESSION["player"]);
-    //verifier si il a gagner
-    calcWin();
-    //savoir quel joueur doit jouer
-    $_SESSION["player"] = ($_SESSION["player"] === 1)?2:1;
-}
-
-//verifier quel joueur gagne si calcwin() a renvoyer un nombre
-$checkWin = (calcWin() === 1 || calcWin() === 2)?true:false;
 generateHead(strval($_GET["page"]), "fr", "UTF-8", "puissance4");
 generateHeader("puissance 4", $_SESSION["player"], $checkWin);
 ?>
@@ -19,10 +6,6 @@ generateHeader("puissance 4", $_SESSION["player"], $checkWin);
     <?php
         generateSection(6, 7, "", "", "visual", "", "graphic", $_SESSION["firstTurn"]);
         generateSection(6, 7, "post", "#", "", "commande", "interactive", $_SESSION["firstTurn"]);
-        //verifier si cest le premier tour
-        if($_SESSION["firstTurn"] === true){
-            $_SESSION["firstTurn"] = false;
-        }
     ?>
 </main>
 <?php
